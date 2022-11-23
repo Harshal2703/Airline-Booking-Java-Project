6eead016fd48b2ab8cc173929b36c3bb7ec070c9
Harshal <harshalchaudhari2703@gmail.com>
2022-11-23 12:30:18 +0530

    updated window icon

--- src/main/java/com/example/airlinesbooking/testMain.java
+++ src/main/java/com/example/airlinesbooking/testMain.java
@@ -3,6 +3,7 @@ package com.example.airlinesbooking;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,6 +18,8 @@ public class testMain extends Application {
     public void start(Stage primaryStage) throws IOException {
         primaryStage.setScene(new Scene(new FXMLLoader(testMain.class.getResource("login.fxml")).load()));
         primaryStage.setResizable(false);
+        Image icon = new Image("E:\\Mini-Project\\AirlinesBooking\\src\\main\\resources\\com\\example\\airlinesbooking\\Icons\\globe.png");
+        primaryStage.getIcons().add(icon);
         primaryStage.show();
     }
 }
